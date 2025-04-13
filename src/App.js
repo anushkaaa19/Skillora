c2a51b92f88c6271ce3492680fdb0b44d8c17032
Anushka Srivastava <anushka.19252406@gmail.com>
2025-04-14 01:34:55 +0530

    setup of frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import {Route,Routes} from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <h1 className="h-screen text-3xl text-center font-bold flex justify-center items-center underline">
-      React + TailwindCSS
-    </h1>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
