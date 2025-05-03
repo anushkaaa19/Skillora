0cdd8942db397a2c6b6193a96c19a15b7315f523
Anushka Srivastava <anushka.19252406@gmail.com>
2025-05-03 09:19:59 +0530

    ui updtae

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import PrivateRoute from "./components/PrivateRoute"
-import { useState } from "react";
-import { useEffect } from "react";
+import Courses from "./pages/Courses";
+import StudyRoom from "./pages/StudyRoom";
+import Leaderboard from "./pages/Leaderboard";
+import AskDoubts from "./pages/AskDoubts";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
+import InstructorDashboard from "./pages/InstructorDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import CourseCreator from "./pages/CourseCreator";
+import Certificate from "./pages/Certificate";
+import React from "react";
 
+const queryClient = new QueryClient();
 
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    localStorage.clear();
-    sessionStorage.clear();
-    console.log("Local and session storage cleared.");
-  }, [])
-
-  return (
-    <div>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/dashboard" element={
-          <PrivateRoute isLoggedIn={isLoggedIn}>
-            <Dashboard />
-
-          </PrivateRoute>
-          } />
-      </Routes>
-    </div>
+const App = function() {
+  return React.createElement(Provider, { store: store },
+    React.createElement(QueryClientProvider, { client: queryClient },
+      React.createElement(TooltipProvider, null,
+        React.createElement(Toaster, null),
+        React.createElement(Sonner, null),
+        React.createElement(BrowserRouter, null,
+          React.createElement(Routes, null,
+            React.createElement(Route, { path: "/", element: React.createElement(Index) }),
+            React.createElement(Route, { path: "/login", element: React.createElement(Login) }),
+            React.createElement(Route, { path: "/signup", element: React.createElement(Signup) }),
+            React.createElement(Route, { path: "/courses", element: React.createElement(Courses) }),
+            React.createElement(Route, { path: "/study-room", element: React.createElement(StudyRoom) }),
+            React.createElement(Route, { path: "/leaderboard", element: React.createElement(Leaderboard) }),
+            React.createElement(Route, { path: "/doubts", element: React.createElement(AskDoubts) }),
+            React.createElement(Route, { path: "/cart", element: React.createElement(Cart) }),
+            React.createElement(Route, { path: "/instructor/dashboard", element: React.createElement(InstructorDashboard) }),
+            React.createElement(Route, { path: "/instructor/courses/create", element: React.createElement(CourseCreator) }),
+            React.createElement(Route, { path: "/student/dashboard", element: React.createElement(StudentDashboard) }),
+            React.createElement(Route, { path: "/certificate/:courseId", element: React.createElement(Certificate) }),
+            React.createElement(Route, { path: "*", element: React.createElement(NotFound) })
+          )
+        )
+      )
+    )
   );
-}
+};
 
 export default App;
